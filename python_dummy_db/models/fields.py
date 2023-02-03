fb70d7c26a1e7620c376d119973af414762b994e
sempervent <jngrant9@gmail.com>
2023-02-03 13:09:42 -0500

    generate based on random_func

--- python_dummy_db/models/fields.py
+++ python_dummy_db/models/fields.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Define generic fields."""
 # pylint: disable=too-few-public-methods
-from typing import Optional, Any, List, Union
+from typing import Optional, Any, List, Union, Callable
+from random import randint
 from uuid import uuid4, UUID
 from pydantic import BaseModel, SecretStr
 
@@ -14,6 +15,8 @@ class Field(BaseModel):
     value: Optional[Any] = None
     data_type: Optional[Any] = None
     relations: Optional[List[Any]] = None
+    values: Optional[List[Any]] = None
+    rand_func: Optional[Callable] = None
 
     def relation(self, new_relation: Any):
         """Create a new relationships object and existing relations.
@@ -28,7 +31,32 @@ class Field(BaseModel):
             self.relations = []
         self.relations.append(new_relation)
 
+    # pylint: disable=too-many-arguments
+    def generate_values(
+        self, N: int = 0, min_num: int = 0, max_num: int = 100,
+        store: bool = True, return_values: bool = False,
+        append: bool = True,
+    ) -> Optional[list]:
+        """Generate N random values from min to max."""
+        if return_values is False and store is False and append is False:
+            return None
+        if N < 1:
+            return None
+        values = []
+        for _ in range(N):
+            values.append(self.random_func(min_num, max_num))
+        if append is True:
+            self.values.extend(values)
+        if store is True and append is False:
+            self.values = values
+        if return_values is True:
+            return values
+        return None
+    # pylint: enable=too-many-arguments
+
 
 class IntegerField(Field):
     """An Integer field."""
     value: int
+    random_func: Callable = randint
+
