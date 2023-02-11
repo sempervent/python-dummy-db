d42366185906ea4ad89e6760ebd1bba4bbad111f
sempervent <jngrant9@gmail.com>
2023-02-10 21:42:15 -0500

    more field tests

--- tests/models/test_fields.py
+++ tests/models/test_fields.py
@@ -6,15 +6,16 @@ from python_dummy_db.models.fields import Field
 
 def test_field_creation():
     """Test that Field works as expected."""
-    int_field = Field(value=3, data_type=int, rand_func=randint)
-    str_field = Field(value='string', data_type=str, rand_func=choice)
+    int_field = Field(name='Integer', data_type=int, rand_func=randint)
+    str_field = Field(name='String', data_type=str, rand_func=choice)
     # assert that created values are Field objects
     assert isinstance(int_field, Field) is True
     assert isinstance(str_field, Field) is True
-    # assert that value and data_type are the correct
-    assert isinstance(int_field.value, int_field.data_type) is True
-    assert isinstance(str_field.value, str_field.data_type) is True
     # ensure random values can be generated
     int_field.generate_values(1, 10, N=3)
+    str_field.generate_values(N=3, seq=['a', 'b', 'c'])
     assert len(int_field.values) == 3
-
+    assert len(str_field.values) == 3
+    # assert that values and data_type are the correct
+    assert int_field.verify() is True
+    assert str_field.verify() is True
