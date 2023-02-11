15d767504e359e2bd8bbe8369bcb05290f54a1d3
sempervent <jngrant9@gmail.com>
2023-02-11 01:35:27 -0500

    add types for seeds

--- python_dummy_db/types.py
+++ python_dummy_db/types.py
@@ -7,3 +7,8 @@ class NoopType:
     """Don't do anything with this class."""
     pass
 # pylint: enable=unnecessary-pass,too-few-public-methods
+
+
+NumericType = (int, float)
+ByteType = (bytes, bytearray)
+SeedType = NumericType + ByteType + (str, type(None))
